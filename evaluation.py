dc2a4cd26803533552a492f62bfab336d074f60a
okamiwaw <okamiwaw@163.com>
2024-05-14 23:36:03 +0800

    opt all-train

--- evaluation.py
+++ evaluation.py
@@ -33,7 +33,7 @@ def get_valid_dataloader(data_type):
                                 collate_fn=val_collate_fn,
                                 shuffle=False,
                                 pin_memory=True,
-                                num_workers=0,
+                                num_workers=2,
                                 )
     return val_dataloader
 
