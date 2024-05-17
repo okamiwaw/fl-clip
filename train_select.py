211643e106963628104220af86f94042bf9c04b4
okamiwaw <okamiwaw@163.com>
2024-05-17 23:54:52 +0800

    opt all-train

--- train_select.py
+++ train_select.py
@@ -36,7 +36,7 @@ def get_train_dataloader(client_id):
                                   collate_fn=train_collate_fn,
                                   shuffle=True,
                                   pin_memory=True,
-                                  num_workers=8,
+                                  num_workers=2,
                                   )
     return train_dataloader
 
@@ -56,7 +56,7 @@ def get_valid_dataloader( data_type):
                                 collate_fn=val_collate_fn,
                                 shuffle=False,
                                 pin_memory=True,
-                                num_workers=8,
+                                num_workers=2,
                                 )
     return val_dataloader
 
