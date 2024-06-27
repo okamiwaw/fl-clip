6b2c3a2a9aab9a89abd57aa64624ae24d54b0293
okamiwaw <okamiwaw@163.com>
2024-06-27 12:46:26 +0800

    opt all-train

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def get_train_dataloader(client_id):
                                   collate_fn=train_collate_fn,
                                   shuffle=True,
                                   pin_memory=True,
-                                  num_workers=2,
+                                  num_workers=0,
                                   )
     return train_dataloader
 
@@ -54,7 +54,7 @@ def get_valid_dataloader(client , data_type):
                                 collate_fn=val_collate_fn,
                                 shuffle=False,
                                 pin_memory=True,
-                                num_workers=2,
+                                num_workers=0,
                                 )
     return val_dataloader
 
